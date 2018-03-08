d46968bff1573e52dec6284ad4040dff93e93715
Vivekananda Moosani <vivekananda_moosani@persistent.com>
2018-03-08 13:15:06 +0530

    First commit - demonstrate istanbul-lib-instrument issue

---
+++ Link.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Link from './Link';
+import renderer from 'react-test-renderer';
+
+/*class Link extends React.Component {
+  render() {
+    return <a></a>
+  }
+}*/
+test('Link changes the class when hovered', () => {
+  const component = renderer.create(
+    <Link page="http://www.facebook.com">Facebook</Link>,
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  // manually trigger the callback
+  tree.props.onMouseEnter();
+  // re-rendering
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  // manually trigger the callback
+  tree.props.onMouseLeave();
+  // re-rendering
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
